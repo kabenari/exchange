6262136f08bc675b9ef1e79828e2e711a93c0b7c
swarupnihinmedia <swarup.baidya@nihinmedia.jp>
2025-01-30 16:33:39 +0530

    done making

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"swarup-exchange/client"
+	"swarup-exchange/mm"
+	"swarup-exchange/server"
+	"time"
+
+	"golang.org/x/exp/rand"
+)
+
+func main() {
+	go server.StartServer()
+	time.Sleep(1 * time.Second)
+
+	c := client.NewClient()
+
+	cfg := mm.Config{
+		UserID:         8,
+		OrderSize:      10,
+		MinSpread:      20,
+		MakeInterval:   1 * time.Second,
+		SeedOffset:     40,
+		ExchangeClient: c,
+		PriceOffset:    10,
+	}
+	maker := mm.NewMakerMaker(cfg)
+
+	maker.Start()
+
+	time.Sleep(2 * time.Second)
+	go marketOrderPlacer(c)
+
+	select {}
+}
+
+func marketOrderPlacer(c *client.Client) {
+	ticker := time.NewTicker(500 * time.Millisecond)
+
+	for {
+		randint := rand.Intn(10)
+		bid := true
+		if randint < 7 {
+			bid = false
+		}
+
+		order := client.PlaceOrderParams{
+			UserID: 7,
+			Bid:    bid,
+			Size:   1,
+		}
+
+		_, err := c.PlaceMarketOrder(&order)
+		if err != nil {
+			panic(err)
+		}
+
+		<-ticker.C
+	}
+}
